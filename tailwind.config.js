352d3b9008a0eb83ceea95f83aac2539d29d0732
Spark <1575110766@qq.com>
2024-08-11 23:06:43 +0800

    安装tailwindcss优化界面

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/view/**/*.ejs',
+    './public/index.html' // 指定需要处理的文件类型
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
